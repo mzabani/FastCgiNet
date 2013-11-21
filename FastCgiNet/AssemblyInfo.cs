553b79b007320af4e97688fb90a3aa2401b756f2
mzabani <mzabani@gmail.com>
2013-11-21 10:42:54 -0200

    - 2 Clause BSD License

--- FastCgiNet/AssemblyInfo.cs
+++ FastCgiNet/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("FastCgiNet")]
-[assembly: AssemblyDescription("Client side FastCgi 100% managed library")]
+[assembly: AssemblyDescription("FastCgi library for Mono and Microsoft .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
@@ -17,15 +17,12 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 //[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+//[assembly: AssemblyKeyFile("fastcginet_public.key")]
 
 [assembly: InternalsVisibleTo("FastCgiNet.Tests")]
-
-// Just for a while
-[assembly: InternalsVisibleTo("Fos.Tests")]
